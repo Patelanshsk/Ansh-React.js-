8ff5e8f0ea30eb2b36b45c87e4d51363f35171af
Ansh Patel <rp342143@gmail.com>
2026-01-06 14:36:34 -0500

    First React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Name from "./name";
+
+function App() {
+  return (
+    <>
+    <Name />
+    <h1>Hello All</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/name.js
@@ -0,0 +1,7 @@
+function Name(){
+    return(
+        <h1>React JS | Ansh Patel</h1>
+    )
+}
+
+export default Name
